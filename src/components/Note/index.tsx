610655e3bcb057975772a908c33fda7cf1d567e0
seoeongeueun <erin9901@gmail.com>
2025-07-17 19:32:40 +0900

    feat: 메모 삭제 확인 모달 추가

--- src/components/Note/index.tsx
+++ src/components/Note/index.tsx
@@ -9,8 +9,7 @@ export default function Note() {
   const [openSettings, setOpenSettings] = useState<boolean>(false);
   const [mode, setMode] = useState<Mode>("default");
   const noteRef = useRef<HTMLDivElement>(null);
-  const note = useNoteStore((s) => s.note);
-  const updateContent = useNoteStore((s) => s.updateContent);
+  const { note, reset, updateContent } = useNoteStore();
 
   useEffect(() => {
     const noteBox = noteRef.current;
@@ -36,6 +35,43 @@ export default function Note() {
   const handleSaveNote = () => {
     setMode("default");
   };
+
+  const handleDeleteNote = (value: boolean) => {
+    setMode("default");
+    if (value) {
+      //TODO: 메모 삭제 로직 추가 필요
+      reset();
+    }
+  };
+
+  //메모 삭제 확인 모달
+  if (mode === "delete")
+    return (
+      <dialog
+        open
+        role="alertdialog"
+        aria-labelledby="delete-note-title"
+        className="pointer-events-auto p-4 fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 h-40 w-50 text-white border border-px rounded-xs border-theme bg-black/70 backdrop-blur-xs flex flex-col items-center justify-center gap-4"
+      >
+        <h3 id="delete-note-title">Delete this note?</h3>
+        <div className="flex flex-row justify-between w-1/2 items-center">
+          <button
+            type="button"
+            onClick={() => handleDeleteNote(true)}
+            aria-label="Confirm delete"
+          >
+            <i aria-hidden="true" className="hn hn-check-circle text-lg"></i>
+          </button>
+          <button
+            type="button"
+            onClick={() => handleDeleteNote(false)}
+            aria-label="Cancel delete"
+          >
+            <i aria-hidden="true" className="hn hn-times-circle text-lg"></i>
+          </button>
+        </div>
+      </dialog>
+    );
 
   return (
     <article
